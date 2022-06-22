2ba31b5c003981227d7a3cb25804dad06005c063
Esleidin Matos <42306667+eymatos@users.noreply.github.com>
2022-06-21 21:07:55 -0400

    Delete AMRegistro directory
    bye

--- AMRegistro/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "cooproco_n";
-$password = "procon01";
- 
-$link = mysql_connect($servername,$username,$password);
-mysql_select_db("cooproco_amr", $link);
-?>
