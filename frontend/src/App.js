6773068be0a8c1d8ef2437af543d2cfafecbe3f9
yasser <esleidinmatos@gmail.com>
2022-07-12 20:21:32 -0400

    Agregando carpeta frontend con react

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <div class="hola"><h1>Hello World</h1></div>
+
+  }
+
+}
+
+export default App;
