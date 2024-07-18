6f5d0af9880868e9ab988b06422cade075fac522
Esleidin Matos <42306667+eymatos@users.noreply.github.com>
2024-07-17 22:22:15 -0400

    Delete backendnew/cooperativa directory

--- backendnew/cooperativa/endpoints/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Ejemplo de una URL básica
-    # Define más URLs según las vistas que tengas en endpoints.views
-]
