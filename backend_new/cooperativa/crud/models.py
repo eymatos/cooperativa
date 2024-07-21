3f6a14ef8585df162c4f9c4ce4c1f6a7b0a5a09c
christopher reyes rosa <c.reyes20@unapec.edu.do>
2024-07-20 21:28:11 -0400

    agregue en el apartado de models.py las opcipones que todavia no esta comppletas como administrador

--- backend_new/cooperativa/crud/models.py
+++ backend_new/cooperativa/crud/models.py
@@ -5,7 +5,14 @@ class Libro(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name='Titulo')
     imagen = models.ImageField(upload_to='imagenes/',null=True, verbose_name='Imagen')
-    description = models.TextField(null=True, verbose_name='Descripcion')
+    descripcion = models.TextField(null=True, verbose_name='Descripcion')
+
+    def __str__(self):
+        fila = "Titulo: " + self.titulo + " - " + " Descripcion: " + self.descripcion
+        return fila
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.delete()
+        super().delete()
 
 class Usuario(models.Model):
     id_user = models.AutoField(primary_key=True)
