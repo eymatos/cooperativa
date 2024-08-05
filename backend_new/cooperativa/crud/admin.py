17b2731627a1249494b1fddfd135cf7df007e17e
Proconsumidor Proconsumidor <proconsumidor62@746BXP3.proconsumidor01.local>
2024-08-05 15:50:37 -0400

    Eliminando proyecto viejo

--- backend_new/cooperativa/crud/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Libro, Pago, TipoPago, Prestamo, TipoPrestamo, Usuario, Ahorro, Retiro
-
-# Register your models here.
-myModels = [Libro, Pago, TipoPago, Prestamo, TipoPrestamo, Usuario, Ahorro, Retiro]
-admin.site.register(myModels)
